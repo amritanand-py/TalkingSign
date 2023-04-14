130aa56abe6ca5ec90b23b8cbc2d9ea6715f8cd3
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2023-04-14 10:14:16 +0530

    initial Commit
    this includes working first page and history page

---
+++ FirstScreen/FirstScreen/ViewControllers/BackwardTranslationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BackwardTranslationViewController.swift
+//  FirstScreen
+//
+//  Created by Amrit Anand on 12/04/23.
+//
+
+import UIKit
+import AVFoundation
+
+
+
+class BackwardTranslationViewController: UIViewController , AVCapturePhotoCaptureDelegate{
+    @IBOutlet weak var screen: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    // MARK: - Navigation
+
+   
+
+}
