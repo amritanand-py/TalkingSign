29b422e836d350b6881cc93d46baeac5aa7da8c4
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2023-08-03 20:09:21 +0530

    Init

---
+++ TalkingSigns/TalkingSigns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkingSigns
+//
+//  Created by Amrit Anand on 03/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
